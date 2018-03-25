3328f63774892c604b73624273196164c11cc9f7
Jim Bean <jmmmbnnn@gmail.com>
2018-03-25 03:39:04 -0700

    Added helper functions common.sh to extract tarball of twrp if twrp.img.tar extracted to recovery.img. Added code to handle if twrp file is a tarball that extracts to the recovery image. Added code to check that twrp recovery file is a .img file. Refactored isEmpty function and extracted code for isEmptyCheck. Extracted prompting for any key to a common function: promptAnyKey. Added additional step to check kaliNethunterKernel/ for nethunter kernel existence and flash it if exists. Changed "Phone" to "Device" as tablets are not phones. Tested functions and scripts and successfully installed Nethunter on Nexus 10 (mantaray).

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 # HELP FUNCTIONS
 
 isProgramInPath () {
-	echo -e "Checking if $1 is installed"
+    echo -e "Checking if $1 is installed"
     command -v $1 >/dev/null 2>&1 && echo -e "$1 FOUND" || { echo -e >&2 "We require $1 but it's not installed/in path.\nAborting."; exit 1; }
     echo -e "Checking if $1 is installed ...DONE\n"
 }
@@ -12,8 +12,13 @@ deleteGitIgnore () {
     rm -rf $1.gitignore
 }
 
+isEmptyCheck () {
+    find $1 -maxdepth 0 -empty | read v
+    return $?
+}
+
 isEmpty () {
-    if find $1 -maxdepth 0 -empty | read v; then echo -e "Dir $1 is empty. Files are missing\nAborting."; exit 1; else echo -e "Dir $1 has files"; fi
+    if isEmptyCheck $1; then echo -e "Dir $1 is empty. Files are missing\nAborting."; exit 1; else echo -e "Dir $1 has files"; fi
 }
 
 isAdbConnected () {
@@ -25,13 +30,13 @@ opoModelCheck () {
     opo16="16gb"
     opo64="64gb"
     if [ -z "$1" ]
-        then
-            echo -e "No model supplied\nAborting."
-            exit 1
-        else
-            if [ "$1" = "$opo16" ] || [ "$1" = "$opo64" ] ; then echo -e "Using $1 userData for flash"; else echo -e "Model argument found but invalid ($1), check the README!!!\nAborting."; exit 1; fi
-            if [ "$1" = "$opo16" ] ; then fUserData="userdata.img"; fi
-            if [ "$1" = "$opo64" ] ; then fUserData="userdata_64G.img"; fi
+    then
+        echo -e "No model supplied\nAborting."
+        exit 1
+    else
+        if [ "$1" = "$opo16" ] || [ "$1" = "$opo64" ] ; then echo -e "Using $1 userData for flash"; else echo -e "Model argument found but invalid ($1), check the README!!!\nAborting."; exit 1; fi
+        if [ "$1" = "$opo16" ] ; then fUserData="userdata.img"; fi
+        if [ "$1" = "$opo64" ] ; then fUserData="userdata_64G.img"; fi
     fi
 }
 
@@ -43,4 +48,60 @@ doCommonChecks () {
     echo -e "Adb connection check"
     isAdbConnected
     echo -e "Adb connection check ...DONE\n"
+}
+
+isFileExtension () {
+    local status
+
+    echo -e "Checking if $1 is $2 file extension"
+    if [[ $1 == *$2 ]]
+    then
+	status=$?
+	echo -e "...YES"
+    else
+	status=$?
+	echo -e "...NO"
+    fi
+    echo -e "Checking if $1 is $2 file extension ...DONE\n"
+
+    return $status
+}
+
+extractTar () {
+    echo -e "Extracting $1"
+    tar -x -f $1 --one-top-level=$twrpImageDir
+    echo -e "Extracting $1 ...DONE\n"
+}
+
+deleteAllTars () {
+    echo -e "Deleting all tars in $1"
+    rm $1
+    echo -e "Deleting all tars in $1 ...DONE\n"
+}
+
+checkTwrpImageExist () {
+    echo -e "Checking TWRP image existence"
+    isEmpty $twrpImageDir
+    echo -e "Checking TWRP image existence ...DONE\n"
+}
+
+checkTwrpImageExistence () {
+    checkTwrpImageExist
+
+    if isFileExtension $twrpImage ".tar"
+    then
+	extractTar $twrpImage
+	deleteAllTars $twrpImageDir*.tar
+    fi
+
+    checkTwrpImageExist
+    if ! isFileExtension $twrpImage ".img"
+    then
+	echo -e "RECOVERY IMAGE IS NOT AN .img FILE!!!\nAborting."
+	exit 1
+    fi
+}
+
+promptAnyKey () {
+    read -n1 -r -p "Press any key when you're ready..." key
 }
--- twrpFlash.sh
+++ twrpFlash.sh
@@ -22,6 +22,12 @@ deleteGitIgnore $nhDir
 nhZip="$nhDir"*
 sdnh="/sdcard/kaliNethunter.zip"
 
+# Kali Nethunter Kernel
+nhkDir=kaliNethunterKernel/
+deleteGitIgnore $nhkDir
+nhkZip="$nhkDir"*
+sdnhk="/sdcard/kaliNethunterKernel.zip"
+
 # INIT
 
 echo -e "NETHUNTER LINUX FLASH (TWRP, SuperSU and Kali Nethunter)\n"
@@ -31,9 +37,9 @@ echo -e "CHECKING PRE-REQUISITES"
 
 doCommonChecks
 
-echo -e "Checking TWRP image existence"
-isEmpty $twrpImageDir
-echo -e "Checking TWRP image existence ...DONE\n"
+isProgramInPath tar
+
+checkTwrpImageExistence
 
 echo -e "Checking SuperSu existence"
 isEmpty $superSuDir
@@ -43,6 +49,17 @@ echo -e "Checking Kali Nethunter zip existence"
 isEmpty $nhDir
 echo -e "Checking Kali Nethunter zip existence ...DONE\n"
 
+echo -e "Checking Kali Nethunter Kernel zip existence"
+isEmptyCheck $nhkDir
+shouldInstallNHK=$(echo $?)
+if [[ $shouldInstallNHK -eq 1 ]]
+then
+    echo -e "Dir $nhkDir has files"
+else
+    echo -e "Dir $nhkDir has no files"
+fi
+echo -e "Checking Kali Nethunter Kernel zip existence ...DONE\n"
+
 echo -e "CHECKING PRE-REQUISITES ...DONE\n"
 sleep 3
 
@@ -50,13 +67,23 @@ sleep 3
 # File Transfer
 echo -e "Sending Kali Nethunter zip to the device"
 adb push -p $nhZip $sdnh 2>/dev/null ||
-	adb push $nhZip $sdnh
+    adb push $nhZip $sdnh
 sleep 3
 echo -e "Sending Kali Nethunter zip to the device ...DONE\n"
 
+if [[ $shouldInstallNHK -eq 1 ]]
+then
+    echo -e "Sending Kali Nethunter Kernel zip to the device"
+    adb push -p $nhkZip $sdnhk 2>/dev/null ||
+	adb push $nhkZip $sdnhk
+    sleep 3
+    echo -e "Sending Kali Nethunter Kernel zip to the device ...DONE\n"
+fi
+
 echo -e "Sending SuperSu zip to the device"
+
 adb push -p $superSuZip $sdSupersu 2>/dev/null ||
-  adb push $superSuZip $sdSupersu
+    adb push $superSuZip $sdSupersu
 sleep 3
 echo -e "Sending SuperSu zip to the device ...DONE\n"
 
@@ -81,7 +108,7 @@ fastboot boot $twrpImage
 sleep 20
 echo -e "Booting into TWRP ...DONE\n"
 
-read -n1 -r -p "Press any key when you're ready..." key
+promptAnyKey
 
 
 # Installing
@@ -94,11 +121,21 @@ sleep 5
 adb shell "twrp install $sdnh"
 echo -e "Installing Kali Linux Nethunter ...DONE\n"
 
+if [[ $shouldInstallNHK -eq 1 ]]
+then
+    echo -e "Kali Nethunter Kernel zip exists and will be installed"
+    echo -e "Press any key when back in TWRP"
+    promptAnyKey
+    echo -e "Installing Kali Linux Nethunter Kernel"
+    sleep 5
+    adb shell "twrp install $sdnhk"
+    echo -e "Installing Kali Linux Nethunter Kernel ...DONE\n"
+fi
 
 # Done!
 echo -e "Rebooting into Kali Linux Nethunter"
 sleep 3
 adb reboot
 
-echo -e "\nEverything is installed. Phone should be booting up! Enjoy Nethunter\n"
+echo -e "\nEverything is installed. Device should be booting up! Enjoy Nethunter\n"
 exit
